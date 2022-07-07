fb5ba93661921ea87a998011504dc2388c6b2d2a
Graquick <84386995+Graquick@users.noreply.github.com>
2022-07-07 19:14:15 +0300

    small fixes

--- .history/pages/posts/markdown_20220707191001.js
+++ .history/pages/posts/markdown_20220707191001.js
@@ -82,7 +82,7 @@ export default function Markdown() {
                         {pos.main}
                     </div>
                 ))}
-            <div className="absolute bottom-20 right-0 w-[230px] xl:w-[200px] xl:left-0 xl:right-0 xl:m-auto xl:mt-12">
+            <div className="absolute bottom-20 right-0 w-[230px] xl:w-[200px] xl:left-0 xl:right-0 xl:m-auto xl:bottom-10">
                 <Footer />
             </div>
             </div>
